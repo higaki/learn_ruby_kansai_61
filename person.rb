a3f03b7639435d8709af2ddde90acfb027ca8881
HIGAKI Masaru <mas.higa@gmail.com>
2014-05-04 09:26:09 +0900

    add a name attribute to a Person class

--- person.rb
+++ person.rb
@@ -4,8 +4,17 @@ RUBY_DESCRIPTION # => "ruby 2.1.1p76 (2014-02-24 revision 45161) [x86_64-darwin1
 
 class Person; end
 
-obj = Person.new                # => #<Person:0x007fa46d013ab0>
+obj = Person.new                # => #<Person:0x007fe0bb811ed0>
 
 obj.class                       # => Person
 Person.superclass               # => Object
 Person.ancestors                # => [Person, Object, Kernel, BasicObject]
+
+
+class Person
+  def initialize name
+    @name = name
+  end
+end
+
+matz = Person.new('matz')       # => #<Person:0x007fe0bb810ff8 @name="matz">

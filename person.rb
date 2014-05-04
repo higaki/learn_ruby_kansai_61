acc0d7ef3c1c529f44deee1df8605bae8cca5472
HIGAKI Masaru <mas.higa@gmail.com>
2014-05-04 10:05:07 +0900

    use the Person object to the key of the Hash

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ RUBY_DESCRIPTION # => "ruby 2.1.1p76 (2014-02-24 revision 45161) [x86_64-darwin1
 
 class Person; end
 
-obj = Person.new                # => #<Person:0x007fc10415d340>
+obj = Person.new                # => #<Person:0x007fd59110d2b8>
 
 obj.class                       # => Person
 Person.superclass               # => Object
@@ -17,7 +17,7 @@ class Person
   end
 end
 
-matz = Person.new('matz')       # => #<Person:0x007fc10415d688 @name="matz">
+matz = Person.new('matz')       # => #<Person:0x007fd59110bf30 @name="matz">
 
 
 class Person
@@ -55,7 +55,7 @@ matz.age                        # => 49
 dhh.age                         # => 34
 
 
-matz.to_s                       # => "#<Person:0x007fc10415d688>"
+matz.to_s                       # => "#<Person:0x007fd59110bf30>"
 matz.method(:to_s)              # => #<Method: Person(Kernel)#to_s>
 
 class Person
@@ -97,3 +97,43 @@ people.sort                     # => [dhh(34), matz(49)]
 
 people.sort_by(&:age)           # => [dhh(34), matz(49)]
 people.sort_by{|p| -p.age}      # => [matz(49), dhh(34)]
+
+
+h = {matz => "Ruby", dhh => "Rails"}
+
+h[matz]                         # => "Ruby"
+h[dhh]                          # => "Rails"
+
+key = Marshal.load(Marshal.dump matz)
+key == matz                     # => true
+h[key]                          # => nil
+
+
+class Person
+  def hash
+    [@name, @born].hash
+  end
+end
+
+matz.hash                       # => 1608110943548551986
+dhh.hash                        # => 3394067242018496055
+
+
+class Person
+  def eql? o
+    [@name, @born].eql? [o.name, o.born]
+  end
+end
+
+key.eql? matz                   # => true
+key.eql? dhh                    # => false
+
+h[matz]                         # => nil
+h[dhh]                          # => nil
+h[key]                          # => nil
+
+h.rehash
+
+h[matz]                         # => "Ruby"
+h[dhh]                          # => "Rails"
+h[key]                          # => "Ruby"
